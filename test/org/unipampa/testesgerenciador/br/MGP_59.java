2786821a1853a63e576f3d1837ebeaad788ee013
Yury Alencar Lima <yuryalencar19@gmail.com>
2017-10-31 04:53:04 -0200

    Remoção de importação não utilizada - MGP-59 (TotalmenteCoberta)

--- test/org/unipampa/testesgerenciador/br/MGP_59.java
+++ test/org/unipampa/testesgerenciador/br/MGP_59.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.unipampa.manipuladorxml.parserxml.br.ParserXML;
 import org.unipampa.tests.evidenceerror.TestingSupport;
