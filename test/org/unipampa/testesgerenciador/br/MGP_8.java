22b3220d802c02d1112a6b467757375d6b24d1c9
Yury Alencar Lima <yuryalencar19@gmail.com>
2017-11-04 05:18:26 -0200

    Adição de um método de teste para os dados inválidos.

--- test/org/unipampa/testesgerenciador/br/MGP_8.java
+++ test/org/unipampa/testesgerenciador/br/MGP_8.java
@@ -47,6 +47,94 @@ public class MGP_8 {
         driver.manage().window().maximize();
     }
 
+    //<editor-fold defaultstate="collapsed" desc="Métodos auxiliares">
+    private void preencherCampos() throws Exception {
+        int cont = 1;
+        Login.autenticar(driver, "testetestezin@gmail.com", "teste123456", url);
+        List<String[]> inputData = parser.extractDataXML("casodeteste", nomesAtributosSubmeterPlano());
+
+        for (String[] inputs : inputData) {
+
+            if (!driver.getTitle().equals("Página Principal - Empreendedor")) {
+                throw new Exception("Verifique o pré-requisito.");
+            }
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"menuSuperior\"]/nav/div/div[2]/ul/li[2]/a")));
+            driver.findElement(By.xpath("//*[@id=\"menuSuperior\"]/nav/div/div[2]/ul/li[2]/a")).click();
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"menuSuperior\"]/nav/div/div[2]/ul/li[2]/ul/li[1]/input")));
+            driver.findElement(By.xpath("//*[@id=\"menuSuperior\"]/nav/div/div[2]/ul/li[2]/ul/li[1]/input")).click();
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("formEquipe:botaoSalvar1")));
+            driver.findElement(By.id("formEquipe:botaoSalvar1")).click();
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.name("formulario_cadastro_projeto:j_idt65")));
+            driver.findElement(By.name("formulario_cadastro_projeto:j_idt65")).click();
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("formulario_cadastro_projeto:empresaProjeto")));
+            driver.findElement(By.id("formulario_cadastro_projeto:empresaProjeto")).sendKeys(inputs[0]);
+            driver.findElement(By.id("formulario_cadastro_projeto:segmentoDeClientes")).sendKeys(inputs[1]);
+            driver.findElement(By.id("formulario_cadastro_projeto:propostaDeValor")).sendKeys(inputs[2]);
+            driver.findElement(By.id("formulario_cadastro_projeto:atividadesChave")).sendKeys(inputs[3]);
+            driver.findElement(By.id("formulario_cadastro_projeto:botaoSalvar2")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")));
+            driver.findElement(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")).click();
+
+            driver.findElement(By.id("tabAnaliseMercado")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("formulario_cadastro_projeto:relacoComClientes")));
+            driver.findElement(By.id("formulario_cadastro_projeto:relacoComClientes")).sendKeys(inputs[4]);
+            driver.findElement(By.id("formulario_cadastro_projeto:parceriasChaves")).sendKeys(inputs[5]);
+            driver.findElement(By.id("formulario_cadastro_projeto:canais")).sendKeys(inputs[6]);
+            driver.findElement(By.id("formulario_cadastro_projeto:recursosPrincipais")).sendKeys(inputs[7]);
+            driver.findElement(By.id("formulario_cadastro_projeto:concorrentes")).sendKeys(inputs[8]);
+            driver.findElement(By.id("formulario_cadastro_projeto:botaoSalvar3")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")));
+            driver.findElement(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")).click();
+
+            driver.findElement(By.id("tabProdutoServico")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"formulario_cadastro_projeto:estagioDeEvolucao\"]")));
+            driver.findElement(By.xpath("//*[@id=\"formulario_cadastro_projeto:estagioDeEvolucao\"]")).click();
+            driver.findElement(By.xpath("//*[@id=\"formulario_cadastro_projeto:estagioDeEvolucao\"]/option[" + cont + "]")).click();
+            driver.findElement(By.id("formulario_cadastro_projeto:tecnologiaProcessos")).sendKeys(inputs[9]);
+            driver.findElement(By.id("formulario_cadastro_projeto:potencialInovacaoTecnologica")).sendKeys(inputs[10]);
+            driver.findElement(By.id("formulario_cadastro_projeto:aplicacoes")).sendKeys(inputs[11]);
+            driver.findElement(By.id("formulario_cadastro_projeto:dificuldadesEsperadas")).sendKeys(inputs[12]);
+            driver.findElement(By.id("formulario_cadastro_projeto:interacaoEmpresaUniversidade")).sendKeys(inputs[13]);
+            driver.findElement(By.id("formulario_cadastro_projeto:interacaoEmpresaComunidadeGoverno")).sendKeys(inputs[14]);
+            driver.findElement(By.id("formulario_cadastro_projeto:infraestrutura")).sendKeys(inputs[15]);
+            driver.findElement(By.id("formulario_cadastro_projeto:botaoSalvar4")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")));
+            driver.findElement(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")).click();
+
+            driver.findElement(By.id("tabGestaoPessoas")).click();
+            driver.findElement(By.id("formulario_cadastro_projeto:participacaoAcionaria")).sendKeys(inputs[16]);
+            driver.findElement(By.id("formulario_cadastro_projeto:potencialEmprego")).sendKeys(inputs[17]);
+            driver.findElement(By.id("formulario_cadastro_projeto:botaoSalvar5")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")));
+            driver.findElement(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")).click();
+
+            driver.findElement(By.id("tabPlanoFinanceiro")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("formulario_cadastro_projeto:fontesDeReceita")));
+            driver.findElement(By.id("formulario_cadastro_projeto:fontesDeReceita")).sendKeys(inputs[18]);
+            driver.findElement(By.id("formulario_cadastro_projeto:estruturaCustos")).sendKeys(inputs[19]);
+            driver.findElement(By.id("formulario_cadastro_projeto:investimentoInicial")).sendKeys(inputs[20]);
+            driver.findElement(By.id("formulario_cadastro_projeto:botaoSalvar6")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")));
+            driver.findElement(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")).click();
+
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("botao_submeter")));
+            driver.findElement(By.id("botao_submeter")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"form_enviar_projeto:j_idt221\"]")));
+            driver.findElement(By.xpath("//*[@id=\"form_enviar_projeto:j_idt221\"]")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSubmeter\"]/div/div/div[3]/input")));
+            driver.findElement(By.xpath("//*[@id=\"modalInfoSubmeter\"]/div/div/div[3]/input")).click();
+            if (driver.findElement(By.xpath("//*[@id=\"formulario_cadastro_projeto:j_idt69:0:notificacaoErroSubmissao\"]")).isDisplayed()) {
+                throw new Exception("Erro ao submeter o projeto");
+            }
+            cont++;
+        }
+    }
+
     /**
      * Método que cria a lista de atributos a serem extraídas do XML com a
      * finalidade de servirem como dados de entrada para os casos de teste
@@ -83,14 +171,16 @@ public class MGP_8 {
         return attributesName;
     }
 
+//</editor-fold>
+
     /**
      * Exemplo de método de teste utilizando reports integrados com o TestLink.
      *
      * @throws Exception
      */
     @Test
-    public void submeterPlanoDeTestesCompleto() throws Exception {
-        System.out.println("Submeter Plano de Testes Completo.");
+    public void submeterPlanoDeTestesCompletoDadosValidos() throws Exception {
+        System.out.println("Submeter Plano de Testes Completo com Dados Válidos.");
         parser = new ParserXML(System.getProperty("user.dir") + System.getProperty("file.separator")
                 + "test" + System.getProperty("file.separator")
                 + "org" + System.getProperty("file.separator")
@@ -98,95 +188,10 @@ public class MGP_8 {
                 + "testesgerenciador" + System.getProperty("file.separator")
                 + "datatests" + System.getProperty("file.separator")
                 + "MGP-8(SubmeterPlanoDadosValidos).xml");
-        List<String[]> inputData = parser.extractDataXML("casodeteste", nomesAtributosSubmeterPlano());
-        int cont = 1;
-        
         try {
-            Login.autenticar(driver, "testetestezin@gmail.com", "teste123456", url);
-            
-            for (String[] inputs : inputData) {
-
-                if (!driver.getTitle().equals("Página Principal - Empreendedor")) {
-                    throw new Exception("Verifique o pré-requisito.");
-                }
-
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"menuSuperior\"]/nav/div/div[2]/ul/li[2]/a")));
-                driver.findElement(By.xpath("//*[@id=\"menuSuperior\"]/nav/div/div[2]/ul/li[2]/a")).click();
-
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"menuSuperior\"]/nav/div/div[2]/ul/li[2]/ul/li[1]/input")));
-                driver.findElement(By.xpath("//*[@id=\"menuSuperior\"]/nav/div/div[2]/ul/li[2]/ul/li[1]/input")).click();
-
-                wait.until(ExpectedConditions.elementToBeClickable(By.id("formEquipe:botaoSalvar1")));
-                driver.findElement(By.id("formEquipe:botaoSalvar1")).click();
-
-                wait.until(ExpectedConditions.elementToBeClickable(By.name("formulario_cadastro_projeto:j_idt65")));
-                driver.findElement(By.name("formulario_cadastro_projeto:j_idt65")).click();
-
-                wait.until(ExpectedConditions.elementToBeClickable(By.id("formulario_cadastro_projeto:empresaProjeto")));
-                driver.findElement(By.id("formulario_cadastro_projeto:empresaProjeto")).sendKeys(inputs[0]);
-                driver.findElement(By.id("formulario_cadastro_projeto:segmentoDeClientes")).sendKeys(inputs[1]);
-                driver.findElement(By.id("formulario_cadastro_projeto:propostaDeValor")).sendKeys(inputs[2]);
-                driver.findElement(By.id("formulario_cadastro_projeto:atividadesChave")).sendKeys(inputs[3]);
-                driver.findElement(By.id("formulario_cadastro_projeto:botaoSalvar2")).click();
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")));
-                driver.findElement(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")).click();
-
-                driver.findElement(By.id("tabAnaliseMercado")).click();
-                wait.until(ExpectedConditions.elementToBeClickable(By.id("formulario_cadastro_projeto:relacoComClientes")));
-                driver.findElement(By.id("formulario_cadastro_projeto:relacoComClientes")).sendKeys(inputs[4]);
-                driver.findElement(By.id("formulario_cadastro_projeto:parceriasChaves")).sendKeys(inputs[5]);
-                driver.findElement(By.id("formulario_cadastro_projeto:canais")).sendKeys(inputs[6]);
-                driver.findElement(By.id("formulario_cadastro_projeto:recursosPrincipais")).sendKeys(inputs[7]);
-                driver.findElement(By.id("formulario_cadastro_projeto:concorrentes")).sendKeys(inputs[8]);
-                driver.findElement(By.id("formulario_cadastro_projeto:botaoSalvar3")).click();
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")));
-                driver.findElement(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")).click();
-
-                driver.findElement(By.id("tabProdutoServico")).click();
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"formulario_cadastro_projeto:estagioDeEvolucao\"]")));
-                driver.findElement(By.xpath("//*[@id=\"formulario_cadastro_projeto:estagioDeEvolucao\"]")).click();
-                driver.findElement(By.xpath("//*[@id=\"formulario_cadastro_projeto:estagioDeEvolucao\"]/option["+cont+"]")).click();
-                driver.findElement(By.id("formulario_cadastro_projeto:tecnologiaProcessos")).sendKeys(inputs[9]);
-                driver.findElement(By.id("formulario_cadastro_projeto:potencialInovacaoTecnologica")).sendKeys(inputs[10]);
-                driver.findElement(By.id("formulario_cadastro_projeto:aplicacoes")).sendKeys(inputs[11]);
-                driver.findElement(By.id("formulario_cadastro_projeto:dificuldadesEsperadas")).sendKeys(inputs[12]);
-                driver.findElement(By.id("formulario_cadastro_projeto:interacaoEmpresaUniversidade")).sendKeys(inputs[13]);
-                driver.findElement(By.id("formulario_cadastro_projeto:interacaoEmpresaComunidadeGoverno")).sendKeys(inputs[14]);
-                driver.findElement(By.id("formulario_cadastro_projeto:infraestrutura")).sendKeys(inputs[15]);
-                driver.findElement(By.id("formulario_cadastro_projeto:botaoSalvar4")).click();
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")));
-                driver.findElement(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")).click();
-
-                driver.findElement(By.id("tabGestaoPessoas")).click();
-                driver.findElement(By.id("formulario_cadastro_projeto:participacaoAcionaria")).sendKeys(inputs[16]);
-                driver.findElement(By.id("formulario_cadastro_projeto:potencialEmprego")).sendKeys(inputs[17]);
-                driver.findElement(By.id("formulario_cadastro_projeto:botaoSalvar5")).click();
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")));
-                driver.findElement(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")).click();
-
-                driver.findElement(By.id("tabPlanoFinanceiro")).click();
-                wait.until(ExpectedConditions.elementToBeClickable(By.id("formulario_cadastro_projeto:fontesDeReceita")));
-                driver.findElement(By.id("formulario_cadastro_projeto:fontesDeReceita")).sendKeys(inputs[18]);
-                driver.findElement(By.id("formulario_cadastro_projeto:estruturaCustos")).sendKeys(inputs[19]);
-                driver.findElement(By.id("formulario_cadastro_projeto:investimentoInicial")).sendKeys(inputs[20]);
-                driver.findElement(By.id("formulario_cadastro_projeto:botaoSalvar6")).click();
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")));
-                driver.findElement(By.xpath("//*[@id=\"modalInfoSalvar\"]/div/div/div[3]/input")).click();
-
-                wait.until(ExpectedConditions.elementToBeClickable(By.id("botao_submeter")));
-                driver.findElement(By.id("botao_submeter")).click();
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"form_enviar_projeto:j_idt221\"]")));
-                driver.findElement(By.xpath("//*[@id=\"form_enviar_projeto:j_idt221\"]")).click();
-                wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"modalInfoSubmeter\"]/div/div/div[3]/input")));
-                driver.findElement(By.xpath("//*[@id=\"modalInfoSubmeter\"]/div/div/div[3]/input")).click();
-                if (driver.findElement(By.xpath("//*[@id=\"formulario_cadastro_projeto:j_idt69:0:notificacaoErroSubmissao\"]")).isDisplayed()) {
-                    throw new Exception("Erro para submeter um projeto sendo que para isto foi utilizado todos os dados válidos e todos os campos preenchidos.");
-                }
-                cont++;
-            }
-
-//            Connection.updateResults("Submeter plano de negócio Completo.", null,
-//                    TestLinkAPIResults.TEST_PASSED, TESTLINK_KEY);
+            preencherCampos();
+            Connection.updateResults("Submeter plano de negócio Completo.", null,
+                    TestLinkAPIResults.TEST_PASSED, TESTLINK_KEY);
         } catch (Exception e) {
 
             TestingSupport.saveScreenshotError(driver, System.getProperty("user.dir") + System.getProperty("file.separator")
@@ -196,9 +201,49 @@ public class MGP_8 {
                     + "testesgerenciador" + System.getProperty("file.separator")
                     + "evidenciaserro", "Submeter Plano de Testes Completo");
 
-//            Connection.updateResults("Submeter plano de negócio Completo.", e.getMessage(),
-//                    TestLinkAPIResults.TEST_FAILED, TESTLINK_KEY);
+            Connection.updateResults("Submeter plano de negócio Completo.", e.getMessage() + ". Sendo o mesmo"
+                    + " preenchendo todos os campos utilizando valores válidos.",
+                    TestLinkAPIResults.TEST_FAILED, TESTLINK_KEY);
             Assert.fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Exemplo de método de teste utilizando reports integrados com o TestLink.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void submeterPlanoDeTestesDadosInvalidos() throws Exception {
+        System.out.println("Submeter Plano de Testes com Dados inválidos.");
+        parser = new ParserXML(System.getProperty("user.dir") + System.getProperty("file.separator")
+                + "test" + System.getProperty("file.separator")
+                + "org" + System.getProperty("file.separator")
+                + "unipampa" + System.getProperty("file.separator")
+                + "testesgerenciador" + System.getProperty("file.separator")
+                + "datatests" + System.getProperty("file.separator")
+                + "MGP-8(SubmeterPlanoDadosInvalidos).xml");
+        try {
+            preencherCampos();
+
+            TestingSupport.saveScreenshotError(driver, System.getProperty("user.dir") + System.getProperty("file.separator")
+                    + "test" + System.getProperty("file.separator")
+                    + "org" + System.getProperty("file.separator")
+                    + "unipampa" + System.getProperty("file.separator")
+                    + "testesgerenciador" + System.getProperty("file.separator")
+                    + "evidenciaserro", "Submeter plano de negócio com empreendedores possuindo cadastro incompleto");
+
+            Connection.updateResults("Submeter plano de negócio com empreendedores possuindo cadastro incompleto.", "Foi possível submeter o plano"
+                    + "mesmo utilizando dados inválidos - Lista com os dados fornecidas"
+                    + "no MantisBT como referência.",
+                    TestLinkAPIResults.TEST_FAILED, TESTLINK_KEY);
+        
+            Assert.fail("Foi possível submeter o plano mesmo utilizando dados inválidos - Lista com os dados fornecidas"
+                    + "no MantisBT como referência.");
+            
+        } catch (Exception e) {
+            Connection.updateResults("Submeter plano de negócio com empreendedores possuindo cadastro incompleto.", null,
+                    TestLinkAPIResults.TEST_PASSED, TESTLINK_KEY);
         }
     }
 
