fb3b850004eb2b99749e1da3b65be0769dc0c348
Lucas Martins <lgatts@gmail.com>
2017-11-07 15:08:17 -0200

    2 novos Testes MGP_53

--- test/org/unipampa/testesgerenciador/br/MGP_53.java
+++ test/org/unipampa/testesgerenciador/br/MGP_53.java
@@ -5,7 +5,6 @@
  */
 package org.unipampa.testesgerenciador.br;
 
-
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -56,7 +55,9 @@ public class MGP_53 {
     @Test
     public void logarEmailSenhaValidos() throws Exception {
 
-        System.out.println("Logar com Email e Senha Válidos");
+        String nomeCasoTeste = "Logar com usuário e senhas Válidos";
+
+        System.out.println(nomeCasoTeste);
         parser = new ParserXML(System.getProperty("user.dir") + System.getProperty("file.separator")
                 + "test" + System.getProperty("file.separator")
                 + "org" + System.getProperty("file.separator")
@@ -77,8 +78,12 @@ public class MGP_53 {
                 driver.findElement(By.xpath("//*[@id='formularioDeLogin:senhaInput']")).sendKeys(data[1]);
                 driver.findElement(By.xpath("//*[@id='formularioDeLogin:botaoLogin']/span[2]")).click();
 
-                Connection.updateResults("Logar com usuário e senhas válidos", null,
-                        TestLinkAPIResults.TEST_PASSED, TESTLINK_KEY);
+                if (driver.getCurrentUrl().equalsIgnoreCase("http://192.168.130.102:8080/GerenciadorPampatec/view/empreendedor/homeEmpreendedor.jsf")) {
+                    Connection.updateResults(nomeCasoTeste, null,
+                            TestLinkAPIResults.TEST_PASSED, TESTLINK_KEY);
+                } else {
+                    throw new Exception("Login Falhou!");
+                }
             }
 
         } catch (Exception e) {
@@ -88,9 +93,9 @@ public class MGP_53 {
                     + "org" + System.getProperty("file.separator")
                     + "unipampa" + System.getProperty("file.separator")
                     + "testesgerenciador" + System.getProperty("file.separator")
-                    + "evidenciaserro", "Logar com usuário e senhas válidos");
+                    + "evidenciaserro", nomeCasoTeste);
 
-            Connection.updateResults("Logar com usuário e senhas válidos", null,
+            Connection.updateResults(nomeCasoTeste, null,
                     TestLinkAPIResults.TEST_FAILED, TESTLINK_KEY);
 
             Assert.fail(e.getMessage());
@@ -100,7 +105,9 @@ public class MGP_53 {
     @Test
     public void logarEmailSenhaInvalidos() throws Exception {
 
-        System.out.println("Logar com Email e Senha Inválidos");
+        String nomeCasoTeste = "Logar com usuário e senhas Inválidos";
+
+        System.out.println(nomeCasoTeste);
         parser = new ParserXML(System.getProperty("user.dir") + System.getProperty("file.separator")
                 + "test" + System.getProperty("file.separator")
                 + "org" + System.getProperty("file.separator")
@@ -124,7 +131,7 @@ public class MGP_53 {
                 driver.findElement(By.xpath("//*[@id='formularioDeLogin:botaoLogin']/span[2]")).click();
 
                 if (driver.findElement(By.xpath("//*[@id='formularioDeLogin']/div[1]/span")) != null) {
-                    Connection.updateResults("Logar com usuário e senhas inválidos", null,
+                    Connection.updateResults(nomeCasoTeste, null,
                             TestLinkAPIResults.TEST_PASSED, TESTLINK_KEY);
                 } else {
                     throw new Exception("ERRO AO LOGAR COM EMAIL E SENHA INVÁLIDOS.");
@@ -138,9 +145,99 @@ public class MGP_53 {
                     + "org" + System.getProperty("file.separator")
                     + "unipampa" + System.getProperty("file.separator")
                     + "testesgerenciador" + System.getProperty("file.separator")
-                    + "evidenciaserro", "Logar com usuário e senhas Inválidos");
+                    + "evidenciaserro", nomeCasoTeste);
 
-            Connection.updateResults("Logar com usuário e senhas inválidos", null,
+            Connection.updateResults(nomeCasoTeste, null,
+                    TestLinkAPIResults.TEST_FAILED, TESTLINK_KEY);
+
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void logarEmailSenhaVazios() throws Exception {
+
+        String nomeCasoTeste = "Logar com usuário e senhas vazios";
+
+        System.out.println(nomeCasoTeste);
+
+        try {
+
+            driver.manage().deleteAllCookies();
+            driver.get(url);
+            driver.findElement(By.xpath("//*[@id='formularioDeLogin:emailInput']")).sendKeys("");
+            driver.findElement(By.xpath("//*[@id='formularioDeLogin:senhaInput']")).sendKeys("");
+            driver.findElement(By.xpath("//*[@id='formularioDeLogin:botaoLogin']/span[2]")).click();
+
+            if (driver.findElement(By.xpath("//*[@id='formularioDeLogin']/div[1]/span")).getText().equalsIgnoreCase("Deve ser preenchido o usuário e senha")) {
+                Connection.updateResults(nomeCasoTeste, null,
+                        TestLinkAPIResults.TEST_PASSED, TESTLINK_KEY);
+            } else {
+                throw new Exception("ERRO AO LOGAR COM EMAIL E SENHA VAZIOS.");
+            }
+
+        } catch (Exception e) {
+
+            TestingSupport.saveScreenshotError(driver, System.getProperty("user.dir") + System.getProperty("file.separator")
+                    + "test" + System.getProperty("file.separator")
+                    + "org" + System.getProperty("file.separator")
+                    + "unipampa" + System.getProperty("file.separator")
+                    + "testesgerenciador" + System.getProperty("file.separator")
+                    + "evidenciaserro", nomeCasoTeste);
+
+            Connection.updateResults(nomeCasoTeste, null,
+                    TestLinkAPIResults.TEST_FAILED, TESTLINK_KEY);
+
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void logarEmailSenhaSQLInjection() throws Exception {
+
+        String nomeCasoTeste = "Logar com usuário e senhas SQL Injection";
+
+        System.out.println(nomeCasoTeste);
+        
+        parser = new ParserXML(System.getProperty("user.dir") + System.getProperty("file.separator")
+                + "test" + System.getProperty("file.separator")
+                + "org" + System.getProperty("file.separator")
+                + "unipampa" + System.getProperty("file.separator")
+                + "testesgerenciador" + System.getProperty("file.separator")
+                + "datatests" + System.getProperty("file.separator")
+                + "MGP-53(SenhaEmailSQLInjection).xml");
+        List<String[]> inputData;
+
+        inputData = parser.extractDataXML("casodeteste", nomesAtributos());
+        driver.manage().deleteAllCookies();
+        driver.get(url);
+
+        try {
+
+            for (String[] data : inputData) {
+                driver.findElement(By.xpath("//*[@id='formularioDeLogin:emailInput']")).sendKeys(data[0]);
+                driver.findElement(By.xpath("//*[@id='formularioDeLogin:senhaInput']")).sendKeys(data[1]);
+                driver.findElement(By.xpath("//*[@id='formularioDeLogin:botaoLogin']/span[2]")).click();
+
+                if (driver.getCurrentUrl().equalsIgnoreCase("http://192.168.130.102:8080/GerenciadorPampatec/view/empreendedor/homeEmpreendedor.jsf")) {
+                    throw new Exception("Login Falhou!");
+
+                } else {
+                    Connection.updateResults(nomeCasoTeste, null,
+                            TestLinkAPIResults.TEST_PASSED, TESTLINK_KEY);
+                }
+            }
+
+        } catch (Exception e) {
+
+            TestingSupport.saveScreenshotError(driver, System.getProperty("user.dir") + System.getProperty("file.separator")
+                    + "test" + System.getProperty("file.separator")
+                    + "org" + System.getProperty("file.separator")
+                    + "unipampa" + System.getProperty("file.separator")
+                    + "testesgerenciador" + System.getProperty("file.separator")
+                    + "evidenciaserro", nomeCasoTeste);
+
+            Connection.updateResults(nomeCasoTeste, null,
                     TestLinkAPIResults.TEST_FAILED, TESTLINK_KEY);
 
             Assert.fail(e.getMessage());
